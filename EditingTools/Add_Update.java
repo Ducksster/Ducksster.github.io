9e0299f8fbd97a76b86300ab642341cad696c697
Ducksster <85276614+Ducksster@users.noreply.github.com>
2026-03-11 11:20:09 -0400

    Small background changes

--- EditingTools/Add_Update.java
+++ EditingTools/Add_Update.java
@@ -6,40 +6,51 @@ import java.util.Scanner;
 public class Add_Update {
     public static void main(String[] args) {
     String code = "";
-        try (Scanner in = new Scanner(new File("index.html")); Scanner input = new Scanner(System.in);) {
+            Scanner input = new Scanner(System.in);
             System.out.println("Input headline:");
             String headline = input.nextLine();
             System.out.println("Input blurb:");
             String blurb = input.nextLine();
-            String state = "Searching";
-            int changed = 0;
+            addUpdate(headline,blurb);
+
+            
+    }
+    public static void addUpdate(String headline, String blurb){
+        int changed = 0;
+        String code = "";
+        String state = "Searching";
+        try (Scanner in = new Scanner(new File("index.html"))) {
             for (String line = in.nextLine(); in.hasNextLine(); line = in.nextLine()) {
                 if (changed != 8){
                     if (state.equals("Header") && line.contains("<h2>")) {
                         code += line + "\n";
-                        code += headline + "\n";
+                        code += "\t\t\t\t\t\t\t\t\t\t\t\t\t" + headline + "\n";
                         headline = in.nextLine();
                         state = "Body";
                         continue;
                     }else if (state.equals("Body") && line.contains("<td valign=\"top\">")) {
                         code += line + "\n";
-                        code += blurb + "\n";
+                        code += "\t\t\t\t\t\t\t\t\t\t\t\t" + blurb + "\n";
                         blurb = in.nextLine();
                         state = "Header";
                         continue;
                     }
                     if (line.contains("<!--First-->")) {
                         state = "Header";
-                        continue;
                     }
                 }
                 code += line + "\n";
+            }
+            if(state.equals("Searching")){
+                System.out.println("Add update failed although did not error");
+                return;
             }
             PrintWriter pw = new PrintWriter("index2.html");
             pw.write(code);
             pw.close();
 
         } catch (Exception e) {
+            System.out.println("Add update failed");
             e.printStackTrace();
             // TODO: handle exception
         }
