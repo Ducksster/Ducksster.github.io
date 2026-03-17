4e58b4b572eef36152f4a9c01449e5e1a4600fb3
Ducksster <85276614+Ducksster@users.noreply.github.com>
2026-03-17 15:30:38 -0400

    Added header tag

--- EditingTools/Add_Update.java
+++ EditingTools/Add_Update.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class Add_Update {
     public static void main(String[] args) {
-    String code = "";
             Scanner input = new Scanner(System.in);
             System.out.println("Input headline:");
             String headline = input.nextLine();
